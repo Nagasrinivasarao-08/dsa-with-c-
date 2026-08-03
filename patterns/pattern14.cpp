24b9754dfc012b7474eefccff757181adc21386f
Nagasrinivasarao-08 <s.eevuri@op.iitg.ac.in>
2026-08-03 18:08:52 +0530

    today task four patterns are completed

---
+++ patterns/pattern14.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+void  pattern14(int n){
+    for(int i = 0; i < n; i++){
+        for(char ch ='A'; ch <= 'A' + i; ch++){
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+int main(){
+    int n = 6;
+    pattern14(n);
+    return 0;
+}
