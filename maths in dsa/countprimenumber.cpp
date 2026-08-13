8327289418c1640c9d0b294f6e61a438a8831d03
Nagasrinivasarao-08 <s.eevuri@op.iitg.ac.in>
2026-08-13 22:30:33 +0530

    count prime

---
+++ maths in dsa/countprimenumber.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int countprimenumber(int n){
+    int count = 0;
+    for(int i = 2; i<=n; i++){
+        bool isprime = true;
+        for(int j = 2; j*j <= i; j++){
+            if(i%j == 0){
+                isprime = false;
+                break;
+            }
+        }
+        if(isprime){
+            count++;
+        }
+    }
+    return count;
+}
+int main(){
+    int n;
+    cin >> n;
+    cout << countprimenumber(n) << endl;
+}
